63d8bfb4d059eac5c59c24cb0d89a4231752c4e5
Vladimir Nesterovsky <vladimir@nesterovsky-bros.com>
2015-12-18 01:11:38 +0200

    Update bulk_insert.sql

--- SQL/Persons/bulk_insert.sql
+++ SQL/Persons/bulk_insert.sql
@@ -1,5 +1,7 @@
 -- Note: {path to output folder} - is a folder where preprocessor output files.
 
+set ansi_warings off
+
 bulk insert Data.PropertyType
   from '{path to output folder}\metadata.txt'
   with(batchsize = 100000, datafiletype = 'widechar');
